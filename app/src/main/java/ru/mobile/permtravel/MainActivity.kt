167781a3001d93d1ebb9b55693391fed848f6de7
orlovanton01 <aov-perm@mail.ru>
2025-02-02 17:59:56 +0500

    Добавление карточки с названием достопримечательности и картинки

--- app/src/main/java/ru/mobile/permtravel/MainActivity.kt
+++ app/src/main/java/ru/mobile/permtravel/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -19,8 +21,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ru.mobile.permtravel.ui.theme.PermTravelTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,7 +45,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainPage(modifier: Modifier = Modifier) {
     Scaffold(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize(),
         topBar = {
             TopAppBar(
                 colors = TopAppBarDefaults.topAppBarColors(
@@ -56,21 +63,38 @@ fun MainPage(modifier: Modifier = Modifier) {
             contentAlignment = Alignment.Center,
             modifier = modifier
                 .padding(innerPadding)
-                .padding(16.dp)
+                .padding(32.dp)
                 .fillMaxSize()
-                .background(Color.White, shape = RoundedCornerShape(16.dp)) // Белый фон и закруглённые углы
+                .background(Color.White, shape = RoundedCornerShape(32.dp)) // Белый фон и закруглённые углы
         ){
-            Greeting("Android", modifier = modifier)
+            Greeting(modifier)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Greeting(modifier: Modifier = Modifier) {
+    Column(
         modifier = modifier
+            .padding(16.dp)
     )
+    {
+        Text(
+            text = "Усьвинские столбы",
+            fontWeight = FontWeight.Bold,
+            fontSize = 22.sp,
+            modifier = modifier
+                .padding(bottom = 5.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.usvinsky_pillars),
+            contentDescription = "Усьвинские столбы",
+            contentScale = ContentScale.Crop
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
