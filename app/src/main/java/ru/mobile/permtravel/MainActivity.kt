9f3bf5721a84b62dd1a23b63be27e9345407893d
orlovanton01 <aov-perm@mail.ru>
2025-02-02 15:59:46 +0500

    Добавление отступов со всех сторон экрана

--- app/src/main/java/ru/mobile/permtravel/MainActivity.kt
+++ app/src/main/java/ru/mobile/permtravel/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -16,7 +18,9 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ru.mobile.permtravel.ui.theme.PermTravelTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,15 +30,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             PermTravelTheme {
                 MainPage()
-                }
             }
         }
     }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainPage() {
+fun MainPage(modifier: Modifier = Modifier) {
     Scaffold(
+        modifier = Modifier.fillMaxSize(),
         topBar = {
             TopAppBar(
                 colors = TopAppBarDefaults.topAppBarColors(
@@ -49,9 +54,13 @@ fun MainPage() {
     ) { innerPadding ->
         Box(
             contentAlignment = Alignment.Center,
-            modifier = Modifier.fillMaxSize()
+            modifier = modifier
+                .padding(innerPadding)
+                .padding(16.dp)
+                .fillMaxSize()
+                .background(Color.White, shape = RoundedCornerShape(16.dp)) // Белый фон и закруглённые углы
         ){
-            Greeting("Android", modifier = Modifier.padding(innerPadding))
+            Greeting("Android", modifier = modifier)
         }
     }
 }
