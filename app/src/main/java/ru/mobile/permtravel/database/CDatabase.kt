d2a7963b296d05dcaa76738d998ce014ed278cf7
orlovanton01 <aov-perm@mail.ru>
2025-03-21 17:38:29 +0500

    Обновление БД

--- app/src/main/java/ru/mobile/permtravel/database/CDatabase.kt
+++ app/src/main/java/ru/mobile/permtravel/database/CDatabase.kt
@@ -11,7 +11,7 @@ import ru.mobile.permtravel.model.Author
 import ru.mobile.permtravel.model.CPlace
 import ru.mobile.permtravel.model.Post
 
-@Database(entities = [CPlace::class, Author::class, Post::class], version = 13)
+@Database(entities = [CPlace::class, Author::class, Post::class], version = 14)
 abstract class CDatabase : RoomDatabase() {
     abstract fun daoPlaces(): CDAOPlaces
     abstract fun daoAuthors(): CDAOAuthors
