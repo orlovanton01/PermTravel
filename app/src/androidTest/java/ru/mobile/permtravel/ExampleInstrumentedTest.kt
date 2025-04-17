21e6a226f776f0f2d212e62da72e1e89eb1d4d61
orlovanton01 <aov-perm@mail.ru>
2025-04-17 18:23:52 +0500

    Добавление Unit-теста проверки того, что с сервера приходит 10 записей из БД

--- app/src/androidTest/java/ru/mobile/permtravel/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ru/mobile/permtravel/ExampleInstrumentedTest.kt
@@ -1,24 +1,34 @@
 package ru.mobile.permtravel
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
+import ru.mobile.permtravel.database.CDatabase
+import ru.mobile.permtravel.repositories.CRepositoryPlaces
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class PlacesRealDatabaseTest {
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("ru.mobile.permtravel", appContext.packageName)
+    fun testPlacesCountAfterServerUpdate() = runBlocking {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+
+        // Получаем реальную БД и репозиторий
+        val db = CDatabase.getDatabase(context)
+        val repository = CRepositoryPlaces(context)
+
+        // 1. Ждём загрузку данных с сервера
+        repository.updatePlacesFromServer()
+
+        // 2. Только теперь читаем и проверяем
+        val places = db.daoPlaces().getAll().first()
+
+        // 3. Проверка: их должно быть 10
+        assertEquals(10, places.size)
     }
 }
